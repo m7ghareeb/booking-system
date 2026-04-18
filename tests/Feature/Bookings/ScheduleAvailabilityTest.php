6adaf67fd2cbec314a93b85d5345c89bd3f03fbc
Mohamed Ghareeb <mohamed.ghareeb725@gmail.com>
2026-04-19 00:49:38 +0200

    refactor: reorganize ScheduleAvailabilityTest for clarity and consistency; enhance availability checks

--- tests/Feature/Bookings/ScheduleAvailabilityTest.php
+++ tests/Feature/Bookings/ScheduleAvailabilityTest.php
@@ -8,154 +8,156 @@ use App\Services\ScheduleAvailabilityService;
 use Carbon\Carbon;
 use Spatie\Period\Period;
 
-it('returns employee daily availability', function () {
-    Carbon::setTestNow(Carbon::parse('1st April 2026'));
+describe('Schedule Availability', function () {
 
-    $employee = Employee::factory()
-        ->has(Schedule::factory()->state([
-            'starts_date' => Carbon::now()->startOfDay(),
-            'ends_date'   => Carbon::now()->addYear()->endOfDay(),
-        ]))
-        ->create();
+    it('returns daily availability based on default schedule', function () {
+        Carbon::setTestNow(Carbon::parse('1st April 2026'));
 
-    $service = Service::factory()->create([
-        'duration' => 30,
-    ]);
+        $employee = Employee::factory()
+            ->has(Schedule::factory()->state([
+                'starts_date' => Carbon::now()->startOfDay(),
+                'ends_date'   => Carbon::now()->addYear()->endOfDay(),
+            ]))
+            ->create();
 
-    $availability = (new ScheduleAvailabilityService($employee, $service))
-        ->forPeriod(
-            Carbon::now()->startOfDay(),
-            Carbon::now()->endOfDay()
-        );
+        $service = Service::factory()->create([
+            'duration' => 30,
+        ]);
 
-    expect($availability->current())
-        ->toBeInstanceOf(Period::class)
-        ->startsAt(now()->setTimeFromTimeString('09:00'))
-        ->toBeTrue()
-        ->endsAt(now()->setTimeFromTimeString('16:30'))
-        ->toBeTrue();
-});
+        $availability = (new ScheduleAvailabilityService($employee, $service))
+            ->forPeriod(
+                Carbon::now()->startOfDay(),
+                Carbon::now()->endOfDay()
+            );
 
-it('returns employee different daily availability', function () {
-    Carbon::setTestNow(Carbon::parse('monday 1st April 2026')); // monday 1st April 2026
+        expect($availability->current())
+            ->toBeInstanceOf(Period::class)
+            ->startsAt(now()->setTimeFromTimeString('09:00'))
+            ->toBeTrue()
+            ->endsAt(now()->setTimeFromTimeString('16:30'))
+            ->toBeTrue();
+    });
 
-    $employee = Employee::factory()
-        ->has(Schedule::factory()->state([
-            'starts_date'       => Carbon::now()->startOfDay(),
-            'ends_date'         => Carbon::now()->addYear()->endOfDay(),
-            'monday_starts_at'  => '12:00', // monday 1st April 2026
-            'monday_ends_at'    => '18:00', // monday 1st April 2026
-            'tuesday_starts_at' => '09:00', // tuesday 2nd April 2026
-            'tuesday_ends_at'   => '17:00', // tuesday 2nd April 2026
-        ]))
-        ->create();
+    it('returns availability per day based on weekday schedule', function () {
+        Carbon::setTestNow(Carbon::parse('monday 1st April 2026')); // monday 1st April 2026
 
-    $service = Service::factory()->create([
-        'duration' => 30,
-    ]);
+        $employee = Employee::factory()
+            ->has(Schedule::factory()->state([
+                'starts_date'       => Carbon::now()->startOfDay(),
+                'ends_date'         => Carbon::now()->addYear()->endOfDay(),
+                'monday_starts_at'  => '12:00', // monday 1st April 2026
+                'monday_ends_at'    => '18:00', // monday 1st April 2026
+                'tuesday_starts_at' => '09:00', // tuesday 2nd April 2026
+                'tuesday_ends_at'   => '17:00', // tuesday 2nd April 2026
+            ]))
+            ->create();
 
-    $availability = (new ScheduleAvailabilityService($employee, $service))
-        ->forPeriod(
-            Carbon::now()->startOfDay(),
-            Carbon::now()->addDay()->endOfDay()
-        );
+        $service = Service::factory()->create([
+            'duration' => 30,
+        ]);
 
-    $mondayAvailability = $availability->current(); // monday 1st April 2026
+        $availability = (new ScheduleAvailabilityService($employee, $service))
+            ->forPeriod(
+                Carbon::now()->startOfDay(),
+                Carbon::now()->addDay()->endOfDay()
+            );
 
-    expect($mondayAvailability)
-        ->toBeInstanceOf(Period::class)
-        ->startsAt(now()->setTimeFromTimeString('12:00'))
-        ->toBeTrue()
-        ->endsAt(now()->setTimeFromTimeString('17:30'))
-        ->toBeTrue();
+        $mondayAvailability = $availability->current(); // monday 1st April 2026
 
-    $availability->next(); // to switch to tuesday 2nd April 2026
+        expect($mondayAvailability)
+            ->toBeInstanceOf(Period::class)
+            ->startsAt(now()->setTimeFromTimeString('12:00'))
+            ->toBeTrue()
+            ->endsAt(now()->setTimeFromTimeString('17:30'))
+            ->toBeTrue();
 
-    $tuesdayAvailability = $availability->current(); // tuesday 2nd April 2026
+        $availability->next(); // to switch to tuesday 2nd April 2026
 
-    expect($tuesdayAvailability)
-        ->toBeInstanceOf(Period::class)
-        ->startsAt(now()->addDay()->setTimeFromTimeString('09:00'))
-        ->toBeTrue()
-        ->endsAt(now()->addDay()->setTimeFromTimeString('16:30'))
-        ->toBeTrue();
-});
+        $tuesdayAvailability = $availability->current(); // tuesday 2nd April 2026
 
-it('returns employee daily availability with exclusions', function () {
-    Carbon::setTestNow(Carbon::parse('1st April 2026'));
+        expect($tuesdayAvailability)
+            ->toBeInstanceOf(Period::class)
+            ->startsAt(now()->addDay()->setTimeFromTimeString('09:00'))
+            ->toBeTrue()
+            ->endsAt(now()->addDay()->setTimeFromTimeString('16:30'))
+            ->toBeTrue();
+    });
 
-    $employee = Employee::factory()
-        ->has(Schedule::factory()->state([
-            'starts_date' => Carbon::now()->startOfDay(),
-            'ends_date'   => Carbon::now()->addYear()->endOfDay(),
-        ]))
-        // Exclude the whole day of 2nd April 2026
-        ->has(ScheduleExclusion::factory()->state([
-            'starts_at' => Carbon::now()->addDay()->startOfDay(),
-            'ends_at'   => Carbon::now()->addDay()->endOfDay(),
-        ]))
-        // Exclude 1 hour from 12:00 to 13:00
-        ->has(ScheduleExclusion::factory()->state([
-            'starts_at' => Carbon::now()->setTimeFromTimeString('12:00'),
-            'ends_at'   => Carbon::now()->setTimeFromTimeString('13:00'),
-        ]))
-        ->create();
+    it('returns availability excluding full-day and partial exclusions', function () {
+        Carbon::setTestNow(Carbon::parse('1st April 2026'));
 
-    $service = Service::factory()->create([
-        'duration' => 30,
-    ]);
+        $employee = Employee::factory()
+            ->has(Schedule::factory()->state([
+                'starts_date' => Carbon::now()->startOfDay(),
+                'ends_date'   => Carbon::now()->addYear()->endOfDay(),
+            ]))
+            // Exclude the whole day of 2nd April 2026
+            ->has(ScheduleExclusion::factory()->state([
+                'starts_at' => Carbon::now()->addDay()->startOfDay(),
+                'ends_at'   => Carbon::now()->addDay()->endOfDay(),
+            ]))
+            // Exclude 1 hour from 12:00 to 13:00
+            ->has(ScheduleExclusion::factory()->state([
+                'starts_at' => Carbon::now()->setTimeFromTimeString('12:00'),
+                'ends_at'   => Carbon::now()->setTimeFromTimeString('13:00'),
+            ]))
+            ->create();
 
-    $availability = (new ScheduleAvailabilityService($employee, $service))
-        ->forPeriod(
-            Carbon::now()->startOfDay(),
-            Carbon::now()->addDay()->endOfDay()
-        );
+        $service = Service::factory()->create([
+            'duration' => 30,
+        ]);
 
-    expect($availability->current())
-        ->toBeInstanceOf(Period::class)
-        ->startsAt(now()->setTimeFromTimeString('09:00'))
-        ->toBeTrue()
-        ->endsAt(now()->setTimeFromTimeString('11:59'))
-        ->toBeTrue();
+        $availability = (new ScheduleAvailabilityService($employee, $service))
+            ->forPeriod(
+                Carbon::now()->startOfDay(),
+                Carbon::now()->addDay()->endOfDay()
+            );
 
-    $availability->next();
+        expect($availability->current())
+            ->toBeInstanceOf(Period::class)
+            ->startsAt(now()->setTimeFromTimeString('09:00'))
+            ->toBeTrue()
+            ->endsAt(now()->setTimeFromTimeString('11:59'))
+            ->toBeTrue();
 
-    expect($availability->current())
-        ->toBeInstanceOf(Period::class)
-        ->startsAt(now()->setTimeFromTimeString('13:00'))
-        ->toBeTrue()
-        ->endsAt(now()->setTimeFromTimeString('16:30'))
-        ->toBeTrue();
+        $availability->next();
 
-    $availability->next();
+        expect($availability->current())
+            ->toBeInstanceOf(Period::class)
+            ->startsAt(now()->setTimeFromTimeString('13:00'))
+            ->toBeTrue()
+            ->endsAt(now()->setTimeFromTimeString('16:30'))
+            ->toBeTrue();
 
-    expect($availability->valid())
-        ->toBeFalse();
-});
+        $availability->next();
 
-it('returns only availability from the current time with an hour upcoming', function () {
-    Carbon::setTestNow(Carbon::parse('1st April 2026 09:15'));
+        expect($availability->valid())
+            ->toBeFalse();
+    });
 
-    $employee = Employee::factory()
-        ->has(Schedule::factory()->state([
-            'starts_date' => Carbon::now()->startOfDay(),
-            'ends_date'   => Carbon::now()->addYear()->endOfDay(),
-        ]))
-        ->create();
+    it('returns upcoming availability only, skipping past time', function () {
+        Carbon::setTestNow(Carbon::parse('1st April 2026 09:15'));
 
-    $service = Service::factory()->create([
-        'duration' => 30,
-    ]);
+        $employee = Employee::factory()
+            ->has(Schedule::factory()->state([
+                'starts_date' => Carbon::now()->startOfDay(),
+                'ends_date'   => Carbon::now()->addYear()->endOfDay(),
+            ]))
+            ->create();
 
-    $availability = (new ScheduleAvailabilityService($employee, $service))
-        ->forPeriod(
-            Carbon::now()->startOfDay(),
-            Carbon::now()->endOfDay()
-        );
+        $service = Service::factory()->create([
+            'duration' => 30,
+        ]);
 
-    expect($availability->current())
-        ->toBeInstanceOf(Period::class)
-        ->startsAt(now()->setTimeFromTimeString('10:00'))
-        ->toBeTrue();
+        $availability = (new ScheduleAvailabilityService($employee, $service))
+            ->forPeriod(
+                Carbon::now()->startOfDay(),
+                Carbon::now()->endOfDay()
+            );
 
+        expect($availability->current())
+            ->toBeInstanceOf(Period::class)
+            ->startsAt(now()->setTimeFromTimeString('10:00'))
+            ->toBeTrue();
+    });
 });
