385fb8086f799323f9c07d58bfd4eeb750ab110e
Mohamed Ghareeb <mohamed.ghareeb725@gmail.com>
2026-04-19 19:10:56 +0200

    feat: enhance Home page with service and employee links, including visual improvements

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Database\Factories\ServiceFactory;
 use Illuminate\Database\Eloquent\Attributes\Guarded;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -13,6 +14,13 @@ class Service extends Model
 {
     /** @use HasFactory<ServiceFactory> */
     use HasFactory;
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => '$ ' . number_format($value / 100, 2),
+        );
+    }
 
     public function employees(): BelongsToMany
     {
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+
 import BaseLayout from '@/layouts/BaseLayout.vue';
+import { Link } from '@inertiajs/vue3';
+
 defineProps({
     employees: Array,
     services: Array,
@@ -13,13 +16,24 @@ defineOptions({ layout: BaseLayout });
         <div>
             <h1 class="text-xl font-medium">Choose a professional</h1>
             <div class="grid grid-cols-2 md:grid-cols-5 gap-8 mt-6">
-                <div v-for="employee in employees" :key="employee.id">
-                    {{ employee.name }}
-                </div>
+                <Link href="" v-for="employee in employees" :key="employee.id"
+                    class="py-8 px-4 border border-slate-200 rounded-lg shadow-sm flex flex-col items-center justify-center text-center hover:bg-gray-50/75">
+                    <img :src="employee.profile_photo_url" alt="" class="size-14 rounded-full mb-4">
+                    <div class="text-sm font-medium mt-3 text-slate-600">{{ employee.name }}</div>
+                </Link>
             </div>
         </div>
         <div>
-            services
+            <h1 class="text-xl font-medium">Or, Choose a service first</h1>
+            <div class="grid grid-cols-2 md:grid-cols-5 gap-8 mt-6">
+                <Link href="" v-for="service in services" :key="service.id"
+                    class="py-8 px-4 border border-slate-200 rounded-lg shadow-sm flex flex-col items-center justify-center text-center hover:bg-gray-50/75">
+                    <div class="text-sm font-medium mt-3 text-slate-600">{{ service.title }}</div>
+
+                    <div class="text-sm font-medium mt-1 text-slate-400">{{ service.duration }} minutes</div>
+                    <div class="text-sm font-medium mt-3 text-slate-600 bg-slate-200 rounded-lg py-0.5 px-1.5">{{ service.price }}</div>
+                </Link>
+            </div>
         </div>
     </div>
 </template>
